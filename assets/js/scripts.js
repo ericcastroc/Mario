ef838ea7e3a02f743efd39b68587ddf7fc99a3df
Eric Castro <eric-henrique44@hotmail.com>
2022-07-07 21:47:43 -0300

    adicionando score

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,10 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
-const gameOver = document.querySelector('.game-over-soon');
-const exit = document.querySelector('.exit');
-const continueButtom = document.querySelector('.continue');
+const gameOver = document.querySelector('.game-over-soon-before');
+const exit = document.querySelector('.exit-before');
+const continueButtom = document.querySelector('.continue-before');
+var count = document.querySelector('.count')
+var countNumber = 0;
 
 
 continueButtom.addEventListener("click", function(){
@@ -21,14 +23,19 @@ function jump(){
     },500);
 }
 
-const loop = setInterval(() =>{
+const loopScore = setInterval(() =>{
+    countNumber = countNumber + 1;
+    count.innerHTML = countNumber;
+},150);
 
+const loop = setInterval(() =>{
 
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
+
    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-    
+
     pipe.style.animation = 'none';
     pipe.style.left = `${pipePosition}px`;
 
@@ -39,10 +46,16 @@ const loop = setInterval(() =>{
     mario.style.width = '70px';
     mario.style.marginLeft = '50px';
 
+    gameOver.classList.add('game-over-soon');
     gameOver.src = "https://github.com/ericcastroc/Mario/blob/main/image/game-over-soon.png?raw=true";
+    continueButtom.classList.remove('continue-before');
+    continueButtom.classList.add('continue');
     continueButtom.src = "https://github.com/ericcastroc/Mario/blob/main/image/continue.png?raw=true";
+    exit.classList.remove('exit-before');
+    exit.classList.add('exit');
     exit.src ="https://github.com/ericcastroc/Mario/blob/main/image/exit.png?raw=true";
     clearInterval(loop);
+    clearInterval(loopScore);
    }
 
 },10);
